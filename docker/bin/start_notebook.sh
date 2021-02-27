1a1c693007ee0d4e4c8e304ace63cd26eb515468
Maksim Mialin <maxim@myalin.ru>
2021-02-28 00:09:07 +0300

    WIP

--- docker/bin/start_notebook.sh
+++ docker/bin/start_notebook.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -it --rm -v ${PWD}:/home --gpus all -p 6005:6006 -p 8887:8888 finrl
+# docker run --privileged --gpus all -it --rm -v ${PWD}:/home -p 6005:6006 -p 8887:8888 finrl
+docker run --privileged -it --rm -v ${PWD}:/home -p 6005:6006 -p 8887:8888 finrl
+
